d72e5d30879eaae0a78aea4185eefd2bddd642bf
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-06-12 12:20:55 +0200

    🚀 Angular 19 SSR afterNextRender for implementing Browser-Only Logic! 🌐

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  afterRender,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpCacheComponent } from './http-cache/http-cache.component';
 
@@ -8,6 +16,43 @@ import { HttpCacheComponent } from './http-cache/http-cache.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('content') contentRef!: ElementRef;
   title = 'angular-ssr';
+
+  constructor() {
+    //Register a callback to be invoked the next time the application finishes rendering, during the
+    // mixedReadWrite phase.
+    afterNextRender(() => {
+      //!if SSR and we would like to do some LOGIC it is better to use this method instead Angular Hooks
+      const contentHeight = this.contentRef.nativeElement.scrollHeight;
+      console.log('Content height in browser[afterNextRender]:', contentHeight);
+    });
+
+    //Register a callback to be invoked each time the application finishes rendering, during the
+    // mixedReadWrite phase.
+    //! After render will execute every time whenever change detection cycle occurs.
+    afterRender(() => {
+      const contentHeight = this.contentRef.nativeElement.scrollHeight;
+      console.log('Content height in browser[afterRender]:', contentHeight);
+    });
+  }
+
+  ngOnInit(): void {
+    console.log(
+      'this.element[ElementRef]: ',
+      this.contentRef,
+      '<= if SSR Undefined in "ngOnInit()"'
+    ); //"Undefined" in SSR mode
+  }
+
+  ngAfterViewInit(): void {
+    console.log(
+      'this.element[ElementRef]: ',
+      this.contentRef,
+      '<= if SSR it works in "ngAfterViewInit()"'
+    ); //"give us
+  }
+
+  //!Angular gives us a new lifecycle hook to get a 'ViewChild":
 }
